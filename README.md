0be91d11f87637263f0988c7c3df581c25df00b1
Julio Cascoe <Juliocasco@usf.edu>
2026-02-01 16:57:37 -0500

    Updated Name for my Index.html

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my Coding Challenge 2 Document
+
+I made a pricing Assistant Tool for our product "Yankees Beer Holders" We all need one.
--- pricing_assistant.js
+++ pricing_assistant.js
@@ -1,2 +1,24 @@
-console.log("Welcome to Pricing Assistant");
+//Comment
 
+console.log("Welcome to Pricing Assistant Tool");
+
+let productname = "Yankees Beer Holder";
+let costperunit = 9.99;
+let baseprice = 19.99;
+let discountrate = 0.10;
+let SalesTaxRate = 0.07;
+let FixedMonthlyCosts = 199;
+
+let discountedprice = baseprice * (1 - discountrate);
+let finalPriceWithtax = discountedprice * (1 + SalesTaxRate);
+let profitperunit = finalPriceWithtax - costperunit;
+
+let breakevenunits = Math.ceil(FixedMonthlyCosts / profitperunit);
+let isprofitable = profitperunit > 0;
+
+console.log("Product Name: " + productname);
+console.log("Discounted Price: $" + discountedprice.toFixed(2));
+console.log("Final Price with Tax: $" + finalPriceWithtax.toFixed(2));
+console.log("Profit per Unit: $" + profitperunit.toFixed(2));
+console.log("Breakeven Units: " + breakevenunits);
+console.log("per-unit profitability: " + isprofitable);
